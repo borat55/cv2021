300a82eb8d68176cc62fdb2a68d5746f23769055
Boram Kim <boram8655@gmail.com>
2021-12-04 18:38:35 -0300

    CV 2021
    Personal project, to make a CV using HTML, CSS and JavaScript,

---
+++ main.js
@@ -0,0 +1,51 @@
+const menuBtn = document.querySelector(".nav_menuBtn");
+const menu = document.querySelector(".nav-manu");
+const icons = document.querySelector(".nav-icons");
+const abtMyself = document.querySelector(".nav-manu li:first-Child");
+const viewAbtMyself = document.querySelector(".content-wrap")
+const project = document.querySelector(".nav-manu li:nth-Child(2)")
+const viewProject = document.querySelector(".projects")
+const experience = document.querySelector(".nav-manu li:nth-Child(3)")
+const viewExperience = document.querySelector(".work-experience")
+const education = document.querySelector(".nav-manu li:last-child")
+const viewEducation = document.querySelector(".education")
+const HamburgerIcon = document.querySelector(".nav_menuBtn");
+
+
+menuBtn.addEventListener("click", () => {
+    if (window.innerWidth <= 1080) {
+    menu.style.display = "block"
+    icons.style.display = "flex"} 
+})
+
+
+menu.addEventListener("mouseleave", () => {
+    if (window.innerWidth <= 1080) {
+    menu.style.display = "none"
+    icons.style.display = "none"} 
+})
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 1081) {
+        menu.style.display = "flex"
+        icons.style.display = "flex"
+    }
+})
+
+
+abtMyself.addEventListener("click", () => {
+    viewAbtMyself.scrollIntoView({behavior:"smooth"})
+})
+
+project.addEventListener("click",() => {
+    viewProject.scrollIntoView({behavior:"smooth"})
+})
+
+experience.addEventListener("click",() => {
+    viewExperience.scrollIntoView({behavior:"smooth"})
+})
+
+education.addEventListener("click",() => {
+    viewEducation.scrollIntoView({behavior:"smooth"})
+})
+
